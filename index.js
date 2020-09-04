cf68a901b7cdf322d1ed13623b6d34c13fe1c767
SlavicaPaunovic <calepaunovic@gmail.com>
2020-09-04 02:02:42 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ $(".ive").on("mouseover", function(){
 
 $(".ive").fadeToggle(800).fadeIn(1000);
 });
+
+$(".ive1").on("mouseover", function(){
+
+$(".ive1").fadeToggle(800).fadeIn(1000);
+});
+
+$(".ive2").on("mouseover", function(){
+
+$(".ive2").fadeToggle(800).fadeIn(1000);
+});
